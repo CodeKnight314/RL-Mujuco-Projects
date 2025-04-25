da7578b180525a64401e15acdd9820017fde2b7b
CodeKnight314 <tang.ri@northeastern.edu>
2025-04-25 15:24:30 +0100

    Update .sh files

--- ant.sh
+++ ant.sh
@@ -1,1 +1,4 @@
-python3 -m src.main --config src/humanoid/config.yaml --mode TD3 --path outputs/ --model humanoid --train
+python3 -m src.main --config src/ant_model/config.yaml --mode TD3 --path outputs/ant_model/TD3 --model ant --train
+xvfb-run --server-args="-screen 0 1024x768x24" python3 -m src.main --config src/ant_model/config.yaml --mode TD3 --path outputs/ --model ant --weights outputs/ant_model/TD3/
+python3 -m src.main --config src/ant_model/config.yaml --mode SAC --path outputs/ant_model/SAC --model ant --train
+xvfb-run --server-args="-screen 0 1024x768x24" python3 -m src.main --config src/ant_model/config.yaml --mode SAC --path outputs/ --model ant --weights outputs/ant_model/SAC/
--- pusher.sh
+++ pusher.sh
@@ -1,1 +1,4 @@
-python3 -m src.main --config src/pusher/config.yaml --mode TD3 --path outputs/ --model pusher --train
+python3 -m src.main --config src/pusher/config.yaml --mode TD3 --path outputs/pusher/TD3/ --model pusher --train
+xvfb-run --server-args="-screen 0 1024x768x24" python3 -m src.main --config src/pusher/config.yaml --mode TD3 --path outputs/ --model pusher --weights outputs/pusher/TD3/
+python3 -m src.main --config src/pusher/config.yaml --mode SAC --path outputs/ --model pusher --weights outputs/pusher/SAC/ --test
+xvfb-run --server-args="-screen 0 1024x768x24" python3 -m src.main --config src/pusher/config.yaml --mode SAC --path outputs/ --model pusher --weights outputs/pusher/SAC/
